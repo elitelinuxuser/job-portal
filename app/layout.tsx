2912914df6592545791e4ca76ce66a7e45c5db9a
Ramanand Sirvi <ramanandsirvi21@gmail.com>
2025-11-19 10:59:53 +0530

    Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
@@ -32,6 +33,7 @@ export default function RootLayout({
         >
           {children}
           <Toaster />
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
